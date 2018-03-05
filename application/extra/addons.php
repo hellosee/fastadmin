74708a0748dbd39df4b829aa030c152165125cc4
hellosee <1335244575@qq.com>
2018-03-05 17:57:55 +0800

    fastadmin
    fastadmin

---
+++ application/extra/addons.php
@@ -0,0 +1,21 @@
+<?php
+
+return array (
+  'autoload' => false,
+  'hooks' => 
+  array (
+    'enable' => 
+    array (
+      0 => 'example',
+      1 => 'wechat',
+    ),
+    'disable' => 
+    array (
+      0 => 'example',
+      1 => 'wechat',
+    ),
+  ),
+  'route' => 
+  array (
+  ),
+);
---
+++ runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
@@ -0,0 +1,4 @@
+<?php
+//000000000000
+ exit();?>
+a:2:{s:6:"enable";a:2:{i:0;s:23:"\addons\example\Example";i:1;s:21:"\addons\wechat\Wechat";}s:7:"disable";a:2:{i:0;s:23:"\addons\example\Example";i:1;s:21:"\addons\wechat\Wechat";}}
